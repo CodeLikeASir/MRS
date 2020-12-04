24b762a14df0e4b98e9f991634983aa2375ecbae
Jan Meissner <meissnej@hochschule-trier.de>
2020-12-04 03:50:43 +0100

    Improved usability, made all arguments optional
    Updated readme to reflect changes, added help functionality

--- MRS.py
+++ MRS.py
@@ -4,11 +4,47 @@ import json
 import time
 import datetime
 import configparser
-import sys
+import argparse, sys
 from praw.models.listing.mixins import subreddit
 
 def checkDate(first, last, date):
+    print("Checking date")
     return date > first and date < last
+
+def intToDate(value):
+    return datetime.datetime.utcfromtimestamp(value).strftime('%Y-%m-%d')
+
+def dateToInt(date):
+    return time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
+
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--fileName', help='Change the generated file name', default='scrap')
+parser.add_argument('--subr', help='Name of subreddit to search in', default='all')
+parser.add_argument('--mode', help='Modes to filter (more info in readme)', default=0)
+parser.add_argument('--keyword', help='Keyword to search for', default='_')
+parser.add_argument('--start', help='First date to find results for, format YYYY-MM-DD, example: 2020-01-01', default=0)
+parser.add_argument('--end', help='Last date to find results for', default=0)
+parser.add_argument('--nrPosts', help='Number of posts to analyze', default=100)
+
+args=parser.parse_args()
+
+subreddit = args.subr
+mode = args.mode
+keyword = args.keyword
+
+if not args.start == 0:
+    start = dateToInt(args.start)
+else:
+    start = args.start
+
+if not args.end == 0:
+    end = dateToInt(args.end)
+else:
+    end = args.end
+    
+analyzedPosts = args.nrPosts
 
 configParser = configparser.RawConfigParser()   
 configFilePath = r'config.txt'
@@ -17,42 +53,27 @@ configParser.read(configFilePath)
 reddit = praw.Reddit(client_id=configParser.get('api-key', 'client_id'), 
                     client_secret=configParser.get('api-key', 'client_secret'), 
                     user_agent=configParser.get('api-key', 'user_agent'))
- 
-analyzedPosts = 100
-mode = 0
-start = 0
-end = 0
 
-if len(sys.argv) > 5:
-    (_, subreddit, mode, keyword, start, end, analyzedPosts) = sys.argv
-    start = time.mktime(datetime.datetime.strptime(start, "%Y-%m-%d").timetuple())
-    end = time.mktime(datetime.datetime.strptime(end, "%Y-%m-%d").timetuple())
-elif len(sys.argv) > 3:
-    (_, subreddit, mode, keyword, analyzedPosts) = sys.argv
-else:
-    (_, subreddit, mode, keyword) = sys.argv
-    
-mode = int(mode)
-    
 posts = []
-
 print("Starting to look for " + keyword + " in " + subreddit)
+if(start > 0 and end > 0):
+    print("From " + intToDate(start) + " to " + intToDate(end))
 
-if mode == 0:
+if keyword == "_":
+    for post in reddit.subreddit(subreddit).top(limit=int(analyzedPosts)):
+        posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
+elif mode == 0:
     for post in reddit.subreddit(subreddit).top(limit=int(analyzedPosts)):
         if(post.link_flair_text == keyword):
-            if(start > 0 and checkDate(start, end, post.created_utc)):
-                posts.append([post.title, post.score, post.subreddit, post.num_comments, 
-                          datetime.datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d')])
-
-if mode == 1:
+            if(start == 0 or checkDate(start, end, post.created_utc)):
+                posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
+elif mode == 1:
     for post in reddit.subreddit(subreddit).top(limit=int(analyzedPosts)):
         if(keyword in post.title):
-            if(start > 0 and checkDate(start, end, post.created_utc)):
-                posts.append([post.title, post.score, post.subreddit, post.num_comments, 
-                          datetime.datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d')])
+            if(start == 0 or checkDate(start, end, post.created_utc)):
+                posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
 
 print("Found " + str(len(posts)) + " results.")   
 posts = pd.DataFrame(posts,columns=['title', 'score', 'subreddit', 'num_comments', 'created'])
     
-posts.to_csv("scrap.csv", index=False)
+posts.to_csv(args.fileName + ".csv", index=False)
--- readme.txt
+++ readme.txt
@@ -1,11 +1,9 @@
-To use the Reddit scrapper, open a terminal, go to the files location and insert:
-python MRS.py "SUBREDDIT" MODE "KEYWORD" "START_DATE" "END_DATE" NUMBER_OF_ANALYZED_POSTS
+To use the Reddit scrapper, open a terminal, go to the files location and insert
+python MRS.py --help for more information.
 
-SUBREDDIT: name of the subreddit
-MODE: 0 to scan flairs, 1 to scan for keyword in titles
-KEYWORD: Word to look for
-START_DATE: First date for posts to be added, Format: YYYY-MM-DD, example: 2020-01-01
-END_DATE: Last date for posts to be added
-NUMBER_OF_ANALYZED_POSTS (OPTIONAL): Number of analyzed top posts (>= number of results)
+Filter modes:
+0: Filter by flair
+1: Filter by keyword in title
 
-Don't forget the " before and after the subreddit and the keyword
+Possible error solutions:
+- Don't forget the " before and after the subreddit and the keyword
