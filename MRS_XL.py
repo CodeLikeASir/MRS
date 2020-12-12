f68786e1ad475dd54279bd396d8546b1bd0c19f9
Jan Meissner <meissnej@hochschule-trier.de>
2020-12-12 15:52:28 +0100

    Improved usability

--- MRS_XL.py
+++ MRS_XL.py
@@ -8,7 +8,6 @@ import datetime
 
 def getPushshiftData(query, after, before, sub):
     url = 'https://api.pushshift.io/reddit/search/submission/?title='+str(query)+'&size=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
-    print(url)
     r = requests.get(url)
     if not '<html>' in r.text:
         data = json.loads(r.text)
@@ -37,9 +36,9 @@ def collectSubData(subm):
 
 def updateSubs_file():
     upload_count = 0
-    print("input filename of submission file, please add .csv")
+    print("input filename of submission file")
     filename = input()
-    file = filename
+    file = filename + ".csv"
     with open(file, 'w', newline='', encoding='utf-8') as file: 
         a = csv.writer(file, delimiter=',')
         headers = ["Post ID","Title","Url","Author","Score","Publish Date","Total No. of Comments","Permalink","Flair"]
@@ -56,13 +55,9 @@ def dateToInt(date):
 parser=argparse.ArgumentParser()
 
 parser.add_argument('--subr', help='name of subreddit to search in', default='all')
-parser.add_argument('--mode', help='modes to filter (more info in readme.txt)', default=0)
 parser.add_argument('--keyword', help='keyword to search for', default='_')
 parser.add_argument('--after', help='first date to find results for, format YYYY-MM-DD, example: 2020-01-01', default=0)
 parser.add_argument('--before', help='last date to find results for', default=0)
-parser.add_argument('--nrPosts', help='number of posts to analyze', default=100)
-parser.add_argument('--fileName', help='change the generated file name', default='scrap')
-parser.add_argument('--format', help='format of output file, can be csv or json (EXPERIMENTAL)', default='csv')
 
 args=parser.parse_args()
     
@@ -86,19 +81,18 @@ query = args.keyword
 subCount = 0
 subStats = {}
 
-data = getPushshiftData(query, after, before, sub)# Will run until all posts have been gathered 
+data = getPushshiftData(query, after, before, sub)
+totalLength = 0
 
 if data:
-    # from the 'after' date up until before date
     while len(data) > 0:
         for submission in data:
             collectSubData(submission)
             subCount+=1
-        # Calls getPushshiftData() with the created date of the last submission
-        print(len(data))
-        print(str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
+        print("Added results until " + str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
         after = data[-1]['created_utc']
+        totalLength += len(data)
         data = getPushshiftData(query, after, before, sub)
-        
-    print(len(data))
+    
+    print(totalLength)
     updateSubs_file()
