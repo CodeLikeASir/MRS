3b11b2f91e32dc8b7b215525720c89861c01f591
Jan Meissner <meissnej@hochschule-trier.de>
2020-12-12 15:55:03 +0100

    Removed old Reddit scrapper with new pushshift-based one

--- MRS.py
+++ MRS.py
@@ -1,90 +1,98 @@
 import pandas as pd
-import praw
+import requests
 import json
+import csv
+import argparse, sys
 import time
 import datetime
-import configparser
-import argparse, sys
-from praw.models.listing.mixins import subreddit
 
-def checkDate(first, last, date):
-    return start == 0 or (date > first and date < last)
+def getPushshiftData(query, after, before, sub):
+    url = 'https://api.pushshift.io/reddit/search/submission/?title='+str(query)+'&size=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
+    r = requests.get(url)
+    if not '<html>' in r.text:
+        data = json.loads(r.text)
+        return data['data']
+    else:
+        print('error.')
+        return None
 
-def intToDate(value):
-    return datetime.datetime.utcfromtimestamp(value).strftime('%Y-%m-%d')
+def collectSubData(subm):
+    subData = list() #list to store data points
+    title = subm['title']
+    url = subm['url']
+    try:
+        flair = subm['link_flair_text']
+    except KeyError:
+        flair = "NaN"    
+    author = subm['author']
+    sub_id = subm['id']
+    score = subm['score']
+    created = datetime.datetime.fromtimestamp(subm['created_utc']) #1520561700.0
+    numComms = subm['num_comments']
+    permalink = subm['permalink']
+    
+    subData.append((sub_id,title,url,author,score,created,numComms,permalink,flair))
+    subStats[sub_id] = subData
+
+def updateSubs_file():
+    upload_count = 0
+    print("input filename of submission file")
+    filename = input()
+    file = filename + ".csv"
+    with open(file, 'w', newline='', encoding='utf-8') as file: 
+        a = csv.writer(file, delimiter=',')
+        headers = ["Post ID","Title","Url","Author","Score","Publish Date","Total No. of Comments","Permalink","Flair"]
+        a.writerow(headers)
+        for sub in subStats:
+            a.writerow(subStats[sub][0])
+            upload_count+=1
+            
+        print(str(upload_count) + " submissions have been uploaded")
 
 def dateToInt(date):
-    return time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
-
+    return int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
 
 parser=argparse.ArgumentParser()
 
 parser.add_argument('--subr', help='name of subreddit to search in', default='all')
-parser.add_argument('--mode', help='modes to filter (more info in readme.txt)', default=0)
 parser.add_argument('--keyword', help='keyword to search for', default='_')
-parser.add_argument('--start', help='first date to find results for, format YYYY-MM-DD, example: 2020-01-01', default=0)
-parser.add_argument('--end', help='last date to find results for', default=0)
-parser.add_argument('--nrPosts', help='number of posts to analyze', default=100)
-parser.add_argument('--fileName', help='change the generated file name', default='scrap')
-parser.add_argument('--format', help='format of output file, can be csv or json (EXPERIMENTAL)', default='csv')
+parser.add_argument('--after', help='first date to find results for, format YYYY-MM-DD, example: 2020-01-01', default=0)
+parser.add_argument('--before', help='last date to find results for', default=0)
 
 args=parser.parse_args()
-
-subreddit = args.subr
-mode = args.mode
-keyword = args.keyword
-
-if not args.start == 0:
-    start = dateToInt(args.start)
-else:
-    start = args.start
-
-if not args.end == 0:
-    end = dateToInt(args.end)
-else:
-    end = args.end
     
-analyzedPosts = int(args.nrPosts)
+#Subreddit to query
+sub = args.subr
+#before and after dates
+after = "1514764800"  #January 1st 
+before = "1538352000" #October 1st
 
-configParser = configparser.RawConfigParser()   
-configFilePath = r'config.txt'
-configParser.read(configFilePath)
-
-reddit = praw.Reddit(client_id=configParser.get('api-key', 'client_id'), 
-                    client_secret=configParser.get('api-key', 'client_secret'), 
-                    user_agent=configParser.get('api-key', 'user_agent'))
-
-print("Starting to look for " + keyword + " in " + subreddit)
-if(start > 0 and end > 0):
-    print("From " + intToDate(start) + " to " + intToDate(end))
-
-postsAnalyzed = 0
-analyzedPosts = 10000
-posts = []
-
-# rework with: 
-
-if keyword == "_":
-    for post in reddit.subreddit(subreddit).top(limit=analyzedPosts):
-        if(checkDate(start, end, post.created_utc)):
-            posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
-elif int(mode) == 0:
-    print("entered mode 0")
-    for post in reddit.subreddit(subreddit).top(limit=analyzedPosts):
-        if(post.link_flair_text == keyword):
-            if(checkDate(start, end, post.created_utc)):
-                posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
-elif int(mode) == 1:
-    for post in reddit.subreddit(subreddit).search(keyword, sort='relevance'):
-        posts.append([post.title, post.score, post.subreddit, post.num_comments, intToDate(post.created_utc)])
-
-print("Analyzed " + str(postsAnalyzed))
-print("Found " + str(len(posts)) + " results.")   
-posts = pd.DataFrame(posts,columns=['title', 'score', 'subreddit', 'num_comments', 'created'])
-
-if(args.format == "csv"):
-    posts.to_csv(args.fileName + ".csv", index=False)
+if not args.after == 0:
+    after = dateToInt(args.after)
 else:
-    posts.to_json(args.fileName + ".json", default_handler=str)
+    after = args.after
     
-print("Output completed.")
+if not args.before == 0:
+    before = dateToInt(args.before)
+else:
+    before = args.before
+    
+query = args.keyword
+subCount = 0
+subStats = {}
+
+data = getPushshiftData(query, after, before, sub)
+totalLength = 0
+
+if data:
+    while len(data) > 0:
+        for submission in data:
+            collectSubData(submission)
+            subCount+=1
+        print("Added results until " + str(datetime.datetime.fromtimestamp(data[-1]['created_utc'])))
+        after = data[-1]['created_utc']
+        totalLength += len(data)
+        data = getPushshiftData(query, after, before, sub)
+    
+    print(totalLength)
+    updateSubs_file()
